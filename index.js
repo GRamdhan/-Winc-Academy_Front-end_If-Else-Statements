168fad15636a3de6171b4e5ddf60cbc229b60d76
GRamdhan <73478034+GRamdhan@users.noreply.github.com>
2022-06-20 16:27:37 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const age = 14;
+const isFemale = false;
+const driverStatus = "designated";
+
+if (age >= 18) {
+    console.log("Welcome to the club");
+} else {
+    console.log("Minors are not allowed, we are sorry");
+}
+if (isFemale == true) {
+    console.log("Welcome to Ladies Night at our pub");
+} else {
+    console.log("Tonight is ladies night, we can not allow you to enter");
+}
+if (driverStatus == "designated") {
+    console.log("Allowed to drive");
+} else {
+    console.log("Not allowed to drive");
+}
